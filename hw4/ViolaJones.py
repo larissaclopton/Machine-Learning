58a8eb2f72f44e5456a4b436c619da56d92f6514
larissaclopton <cloptla@gmail.com>
2017-02-16 10:05:38 -0600

    fixed merging code, works alright but implementation can be slow

--- hw4/ViolaJones.py
+++ hw4/ViolaJones.py
@@ -4,8 +4,7 @@
 # an implementation of the Viola Jones face detection cascade
 
 import numpy as np
-from PIL import Image, ImageDraw
-import math
+from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import time
@@ -18,14 +17,15 @@ width = 64
 height = 64
 
 # store the iimage representation for all images
-nimages = 500 # the number of images, CHANGE FOR DEBUGGING
-iimages = np.zeros((nimages*2,width*height)) # first half holds face, second half holds background
+nimages = 800 # the number of images
+iimages = np.zeros((nimages*2,width*height)) # first half holds faces, second half holds backgrounds
 
 # store definitions of the rectangles
-nfeatures = 8448 # the number of features, CHANGE FOR DEBUGGING
+nfeatures = 30912 # the number of features
 featuretbl = np.zeros((nfeatures,8,2))
 
-# training set labels
+# training set labels, to use as indicator functions
+# NOTE: actual predictions will be of the form +1/-1
 labels = [1]*nimages + [0]*nimages
 inverted_labels = [0]*nimages + [1]*nimages
 
@@ -33,15 +33,17 @@ inverted_labels = [0]*nimages + [1]*nimages
 # updated after each stage of the classification cascade
 curr_images = [i for i in range(nimages*2)]
 
-# computation of iimage
+# SETUP FUNCTIONS
+
 def iimageRep(img):
+	# compute the integral image representation
         # sum of pixel values in image i in the rectangle extending from (0,0) to (x,y)
         # i.e. sum of all pixels above and to the left
         ii = np.zeros(width*height)
         s = np.zeros(width*height)
 
-        for y in range(width):
-                for x in range(height):
+        for y in range(height):
+                for x in range(width):
                         if x == 0:
                                 s[(y*width)+x] = img[(y*width)+x]
                         else:
@@ -52,28 +54,24 @@ def iimageRep(img):
                                 ii[(y*width)+x] = ii[((y-1)*width)+x] + s[(y*width)+x]
         return ii
 
-# feature set generation
 def featureGenerator():
+	# generate a set of features
 
-	stride = 8 # CHANGE FOR DEBUGGING
-	max_width = 32 # CHANGE FOR DEBUGGING
-	max_height = 16 # CHANGE FOR DEBUGGING
+	stride = 4
+	max_width = 32
+	max_height = 16
 
 	n = 0
 	for i in range(2,max_width,2):
 		for j in range(2,max_height,2):
 			for i2 in range(i,width,stride):
 				for j2 in range(j,height-j,stride):
-					# horizontal rectangles, and flip
+					# horizontal rectangles
 					featuretbl[n,:,:] = np.array([(i2-i,j2-j), (i2,j2-j), (i2-i,j2), (i2, j2), \
 								(i2-i,j2), (i2,j2), (i2-i,j2+j), (i2,j2+j)])
-					###featuretbl[n+1,:,:] = np.array([(i2-i,j2), (i2,j2), (i2-i,j2+j), (i2,j2+j), \
-								###(i2-i,j2-j), (i2,j2-j), (i2-i,j2), (i2, j2)])				
-					# vertical rectangles, and flip
+					# vertical rectangles
 					featuretbl[n+1,:,:] = np.array([(i2-i,j2-j), (i2-i/2,j2-j), (i2-i,j2+j), (i2-i/2,j2+j), \
 								(i2-i/2,j2-j), (i2,j2-j), (i2-i/2,j2+j), (i2,j2+j)])
-					###featuretbl[n+3,:,:] = np.array([(i2-i/2,j2-j), (i2,j2-j), (i2-i/2,j2+j), (i2,j2+j), \
-								###(i2-i,j2-j), (i2-i/2,j2-j), (i2-i,j2+j), (i2-i/2,j2+j)])
 					n += 2
 
 # load each face image, convert to greyscale, and compute iimage representation
@@ -93,9 +91,11 @@ for image in range(nimages):
 # generate the feature set
 featureGenerator()
 
+# TRAINING FUNCTIONS
+
 def computeFeature(img,f):
 	# compute feature value for feature f of image img
-	# intensity of black rectangle - intensity of white rectangle (black always first)
+	# intensity of first rectangle - intensity of second rectangle
 	
 	points = featuretbl[f,:4,:]	
 	x1 = points[0,0]
@@ -121,6 +121,15 @@ def computeFeature(img,f):
 
 	return value1-value2
 
+def predict(f,theta,p,alpha,i):
+
+	if p*computeFeature(curr_images[i],f) < p*theta:
+		predict = 1
+	else:
+		predict = -1
+	return alpha*predict
+
+
 def boost(n):
 	
 	# compute number of negatives in the sample
@@ -135,9 +144,8 @@ def boost(n):
 	final = []
 
 	# iterate until false positive rate below 30%
-	t = 0
 	fp = 1 # initialize false positive rate
-	while fp > 0.3:
+	while fp >= 0.3:
 		print('boosting')
 		# normalize the weights to make a probability distribution
 		w = np.true_divide(w,np.sum(w))
@@ -149,12 +157,11 @@ def boost(n):
 		# update the weights based on the chosen classifier
 		beta = err/(1-err)
 		alpha = np.log(1/beta)
-		e = misses
 		for i in range(n):
-			w[i] = w[i]*(beta**(1-e[i]))
+			w[i] = w[i]*(beta**(1-misses[i]))
 		
 		# update the final classifier with the hypothesis and alpha			
-		if t == 0:
+		if not final:
 			final = [(f,theta,p,alpha)]
 		else:
 			final.append((f,theta,p,alpha))
@@ -162,44 +169,34 @@ def boost(n):
 		# compute the final theta
 		pos_results = [0]*nimages # results of positives with the current final classifier
 		for i in range(nimages):
-			result = np.sum([h[3]*int(h[2]*computeFeature(curr_images[i],h[0]) < h[2]*h[1]) for h in final])
-			pos_results[i] = result
+			pos_results[i] = np.sum([predict(h[0],h[1],h[2],h[3],i) for h in final])
 		final_theta = min(pos_results) 
 
 		# compute the false positive rate
 		false_positives = 0 
 		for i in range(nimages,n):
-			result = np.sum([h[3]*int(h[2]*computeFeature(curr_images[i],h[0]) < h[2]*h[1]) for h in final])
-			if result >= final_theta:
+			if np.sum([predict(h[0],h[1],h[2],h[3],i) for h in final]) >= final_theta:
 				false_positives += 1
 		fp = float(false_positives/negatives) 
-		t += 1
 
 	# determine correctly identified negatives by the final classifier
 	correct_negs = []
 	for i in range(nimages,n):
 		# make prediction with final classifier
-		result = np.sum([h[3]*int(h[2]*computeFeature(curr_images[i],h[0]) < h[2]*h[1]) for h in final]) - final_theta
-		if result < 0: 
+		if np.sum([predict(h[0],h[1],h[2],h[3],i) for h in final]) - final_theta < 0:
 			if not correct_negs:
 				correct_negs = [curr_images[i]]
 			else:
 				correct_negs.append(curr_images[i])	
 
 	# determine the training error of the classifier
-	loss = 0
-	for i in range(n):
-        	result = np.sum([h[3]*int(h[2]*computeFeature(curr_images[i],h[0]) < h[2]*h[1]) for h in final]) - final_theta
-        	predict = int(result > 0)
-        	if predict != labels[curr_images[i]]:
-                	loss += 1
-	train_error = loss/float(nimages*2)
-	
-	print('boosting completed')
+	incorrect_negs = negatives - len(correct_negs)
+	train_error = incorrect_negs/float(nimages*2)
+
 	print(train_error)
 
-	# return the final classifier, final theta, and indices of correct negatives
-	return (final,final_theta,correct_negs)
+	# return the final classifier and indices of correct negatives
+	return (final,correct_negs)
 
 def bestLearner(w,n):
 
@@ -219,13 +216,14 @@ def bestLearner(w,n):
 			p = p_tmp
 			err = err_tmp
 
+
 	# determine misclassifications for the best learner
 	misses = [0]*n # 0 for correct predictions
 	for i in range(n):
 		predict = int(p*computeFeature(curr_images[i],f) < p*theta)
 		if predict != labels[curr_images[i]]:
 			misses[i] = 1 # 1 for incorrect predictions
-	
+
 	return (f,theta,p,err,misses)
 
 def hypothesis(f,w,n):
@@ -237,9 +235,10 @@ def hypothesis(f,w,n):
 	# find indices of the images when sorted by this feature
 	order = np.argsort(values)
 
-	# hold errors and polarity for each value of j
-	errors = [0]*(n)
-	polarity = [1]*(n)
+	# hold best_j with minimum err and its polarity p
+	err = 1
+        p = 0 
+        best_j = 0
 
 	# total weight of + examples
         Tplus = np.sum([labels[curr_images[i]]*w[i] for i in range(n)])
@@ -257,17 +256,15 @@ def hypothesis(f,w,n):
 			Sminus += w[order[j]]
 		
 		# compute the error and polarity by which side is smaller
-		side = int(Splus+Tminus-Sminus > Sminus+Tplus-Splus)
-		if side == 0:
-			polarity[j] = -1 # otherwise, keep at 1
-			errors[j] = Splus+Tminus-Sminus
-		else:
-			errors[j] = Sminus+Tplus-Splus
-
-	# find j with the minimum error and its polarity
-	best_j = errors.index(min(errors))
-	err = errors[best_j]
-	p = polarity[best_j]	
+		err_tmp = min([Splus+Tminus-Sminus, Sminus+Tplus-Splus])
+		if err_tmp < err:
+			err = err_tmp
+			best_j = j
+			side = int(Splus+Tminus-Sminus > Sminus+Tplus-Splus)
+			if side == 0:
+				p = -1
+			else:
+				p = 1
 
 	# set threshold between feature value at ordered index best_j and feature value at ordered index best_j+1
 	if (best_j+1) == n:
@@ -285,19 +282,20 @@ def classifierCascade():
 	n = nimages*2 # run the first round of boosting on all training images
 
 	# iterate until 5% of incorrectly classified negatives remain
-	while n > (nimages + nimages/20):
+	neg_target = nimages/20 # the target number of incorrectly classified negatives
+	while n - nimages > neg_target:
 		print('new cascade stage')
 		ts = time.time()
 		st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 		print(st)		
 
-		classifier, theta, correct_negs = boost(n)
+		classifier, correct_negs = boost(n)
 
 		# add the classifier to the cascade
 		if not classifiers:
-			classifiers = [(classifier,theta)]
+			classifiers = [classifier]
 		else:
-			classifiers.append((classifier,theta))
+			classifiers.append(classifier)
 
 		# remove the correctly identified negatives from curr_images
 		for image in correct_negs:
@@ -307,19 +305,17 @@ def classifierCascade():
 		n -= len(correct_negs)
 		print(n)
 
+	print('classifier cascade complete')
 	return classifiers
 
 # obtain the classifier cascade from the training data
 classifiers = classifierCascade()
 
-classifiers = np.array(classifiers)
-np.save('classifiers', classifiers)
-
-# functions specific to the test data
+# TESTING FUNCTIONS
 
 def computeFeature_test(iimage,f):
         # compute feature value for feature f of the test iimage
-        # intensity of black rectangle - intensity of white rectangle (black always first)
+        # intensity of first rectangle - intensity of second rectangle
        
 	points = featuretbl[f,:4,:]
         x1 = points[0,0]
@@ -345,21 +341,30 @@ def computeFeature_test(iimage,f):
 
         return value1-value2
 
+def predict_test(f,theta,p,alpha,iimage):
+
+        if p*computeFeature_test(iimage,f) < p*theta:
+                predict = 1
+        else:
+                predict = -1
+        return alpha*predict
+
+
 # load the test image
 x = Image.open('class.jpg')
 x = x.convert('L')
+test_width = x.size[0]
+test_height = x.size[1]
+
 im = np.array(x, dtype=np.uint8)
 fig,ax = plt.subplots(1)
 ax.imshow(im, cmap ='gray')
 
-test_width = x.size[0]
-test_height = x.size[1]
-
-###detections = []
+detections = [] # hold coordinates of detected subwindows
 
 # slide a 64x64 window over the test image
-for i in range(width,test_width,16):
-        for j in range(height,test_height,16):
+for i in range(width,test_width,8):
+        for j in range(height,test_height,8):
                 # compute the iimage representation of the patch
                 image = x.copy()
                 selected_patch = image.crop((i-width,j-height,i,j))
@@ -368,9 +373,10 @@ for i in range(width,test_width,16):
 
                 # run through the cascade of classifiers
                 face = True
-                for (classifier,theta) in classifiers:
-                        result = np.sum([h[3]*int(h[2]*computeFeature_test(iimage,h[0]) < h[2]*h[1]) for h in classifier]) - theta
-                        if result < 0:
+                for classifier in classifiers:
+			gamma = 0.15
+			offset  = gamma*np.sum([h[3] for h in classifier])
+                        if np.sum([predict_test(h[0],h[1],h[2],h[3],iimage) for h in classifier]) + offset < 0:
                                 face = False
                                 break
                         # otherwise, continue through the cascade
@@ -379,11 +385,18 @@ for i in range(width,test_width,16):
                 if face:
                         # draw a square
 			window = [(i-64,j-64), (i,j-64), (i-64,j), (i,j)]
-			###detections += [window]
+			detections += [window]
                         rect = patches.Rectangle((i-64,j-64),width,height,linewidth=3,edgecolor='red',facecolor='none')
         		ax.add_patch(rect)
 
-"""# create a dictionary to hold the sets
+fig.savefig('FaceDetectionResults.jpg')
+
+# MERGE DETECTIONS
+
+fig2,ax2 = plt.subplots(1)
+ax2.imshow(im, cmap ='gray')
+
+# create a dictionary to hold the sets
 sets = {}
 
 for index,window in enumerate(detections):
@@ -396,7 +409,8 @@ for index,window in enumerate(detections):
                         overlap = True
                         # iterate through windows to see if overlaps with all of them
                         for element in windows:
-                                if element[1][0] < window[0][0] or element[3][1] < window[1][1]:
+				# requires that the windows overlap by at least half in both orientations
+                                if not (((element[1][0]-window[0][0] >= 32 and element[1][0]-window[0][0] <= 64) or (window[1][0]-element[0][0] >= 32 and window[1][0]-element[0][0] <=64)) and (element[3][1]-window[0][1]>= 32 and element[3][1]-window[0][1] <= 64)):
                                         # the windows do not overlap
                                         overlap = False
                                         break
@@ -421,6 +435,6 @@ for key,windows in sets.iteritems():
         y3 = sum(l)/float(len(l))
 
 	rect = patches.Rectangle((x1,y1),x2-x1,y3-y1,linewidth=2,edgecolor='red',facecolor='none')
-        ax.add_patch(rect)"""
+        ax2.add_patch(rect)
 
-fig.savefig('FaceDetectionResults.jpg')	
+fig2.savefig('FaceDetectionResults-Merged.jpg')	
